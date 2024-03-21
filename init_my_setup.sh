5e008b54d245195abfbca3559df2ab7dfd226c21
A2N <a2n.dev@pm.me>
2024-03-21 15:23:55 +0100

    improve for using with zsh by default

--- init_my_setup.sh
+++ init_my_setup.sh
@@ -1,15 +1,18 @@
-#!/bin/bash
+#!/bin/zsh
+echo "Launch."
 
-# root has to run the script
-if [[ $(id -u -n) != "root" ]]
-    then
-    printf "You need to be root to do this!\nIf you have SUDO installed, then run this script with `sudo ${0}`"
-    exit 1
+RED='\033[0;31m'
+NOCOLOR='\033[0m'
+
+# try if zsh exist if not send error
+if [ "${SHELL##*/}" != 'zsh' ]; then
+  echo -e "${RED}You need to install zsh first and launch this script with it.${NOCOLOR}" >&2
+  exit 1
 fi
 
-echo "Launch package install."
+echo -e "${RED}Launch package install.${NOCOLOR}"
 # init core package
-yay -S bpytop docker docker-compose eza kitty kleopatra neovim neofetch tldr tree zoxide zsh dracut-numlock
+yay -S bpytop docker docker-compose eza kitty kleopatra neovim neofetch tldr tree zoxide dracut-numlock
 
 # init core gui
 yay -S keepassxc nextcloud-client obs-studio obsidian signal-desktop spotify-launcher flameshot
@@ -17,15 +20,23 @@ yay -S keepassxc nextcloud-client obs-studio obsidian signal-desktop spotify-lau
 # init extra package
 yay -S kdeplasma-blurredwallpaper-git kdeplasma-arch-update-notifier-git lolcat noto-fonts noto-fonts-emoji noto-fonts-extra noto-fonts-cjk ttf-noto-nerd screenfetch
 
-echo "Setup docker."
+echo -e "${RED}Setup docker.${NOCOLOR}"
 # post config for docker
-sudo groupadd docker
+echo -e "${RED}Setup grp.${NOCOLOR}"
+sudo groupadd -f docker # -f is here because in most of the case the cmd failed bcs the group already exist
+echo -e "${RED}Setup usermod.${NOCOLOR}"
 sudo usermod -aG docker $USER
-newgrp docker
-echo "Test docker."
+echo -e "${RED}Refresh grp.${NOCOLOR}"
+#newgrp docker
+echo -e "${RED}start docker.${NOCOLOR}"
+sudo systemctl start docker
+echo -e "${RED}Test docker.${NOCOLOR}"
 docker run hello-world
+echo -e "${RED}stop docker.${NOCOLOR}"
+sudo systemctl stop docker.socket
+sudo systemctl stop docker
 
-echo "Init git."
+echo -e "${RED}Init git.${NOCOLOR}"
 # init git
 git config --global commit.gpgsign true
 git config --global user.signingkey "6A2ECC8A396F8A943A109A1E0F11C2A6BF79111E"
@@ -33,45 +44,52 @@ git config --global user.name "Bouteiller [a2n] Alan"
 git config --global user.email "a2n.dev@pm.me"
 git config --global init.defaultBranch main
 
-echo "Install zsh plugins."
+echo -e "${RED}Install zsh and plugins.${NOCOLOR}"
+# delete old install if present
+rm -rf ~/.oh-my-zsh
+# oh my zsh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
 # install default tool or tool plugins
 ## zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 ## zsh-syntax-highlighting
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+## copy config file
+mv ~/.zshrc ~/.zshrc.post-a2n.bak
+cp .zshrc ~/.zshrc
 
-echo "Install volta."
+echo -e "${RED}Install volta.${NOCOLOR}"
 ## volta for node management
 curl https://get.volta.sh | zsh
 source ~/.zshrc
 volta install node
 volta install yarn
 
-echo "Setup host and host.deny."
+echo -e "${RED}Setup host and host.deny.${NOCOLOR}"
 # host deny config
 sudo wget https://hosts.ubuntu101.co.za/superhosts.deny -O /etc/hosts.deny
 sudo mv /etc/hosts /etc/hosts.bak
 sudo wget https://hosts.ubuntu101.co.za/hosts -O /etc/hosts
 
-echo "Setup kitty."
+echo -e "${RED}Setup kitty.${NOCOLOR}"
 # setup kitty
-mv ./kitty/* ~/.config/kitty/
+mv kitty ~/.config/
 kitty +runpy 'from kitty.config import *; print(commented_out_default_config())' > ~/.config/kitty/kitty.conf
 echo "include frappe.conf" | tee -a ~/.config/kitty/kitty.conf
 
-echo "Config nvim."
+echo -e "${RED}Config nvim.${NOCOLOR}"
 # setup nvim
-mv ./nvim ~/.config/
+mv nvim ~/.config/
 git clone --depth 1 https://github.com/wbthomason/packer.nvim ~/.local/share/nvim/site/pack/packer/start/packer.nvim
 
 # ssh
 mkdir ~/.ssh
 
-echo "////////////////////////////////////"
-echo "Don't forget to import your gpg/ssh key."
-echo "Don't forget to do in nvim :"
-echo " - PackerSync"
-echo " - PackerUpdate"
-echo "////////////////////////////////////"
+echo -e "${RED}////////////////////////////////////"
+echo -e "Don't forget to import your gpg/ssh key."
+echo -e "Don't forget to do in nvim :"
+echo -e " - PackerSync"
+echo -e " - PackerUpdate"
+echo -e "////////////////////////////////////${NOCOLOR}"
 
 exit 0
