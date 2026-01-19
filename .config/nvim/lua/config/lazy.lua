2d39cf0f8aaf939c73cd1b5933e48c15f07d525d
Bouteiller [a2n] Alan <a2n.dev@pm.me>
2026-01-19 18:00:23 +0100

    unfold by default

--- .config/nvim/lua/config/lazy.lua
+++ .config/nvim/lua/config/lazy.lua
@@ -392,6 +392,7 @@ vim.api.nvim_create_autocmd('FileType', {
   callback = function()
     vim.wo[0][0].foldexpr = 'v:lua.vim.treesitter.foldexpr()'
     vim.wo[0][0].foldmethod = 'expr'
+    vim.wo[0][0].foldlevel = 99  -- Open all folds by default
     vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
   end
 })
